ef522834082256d098b49a8f5fab996936794135
kennedy kiio <kiiokennedy38@gmail.com>
2022-03-31 16:58:07 +0300

    Updated Index.html and Javascript files
    Support multiple sliders,
    working on scroll to slide/transit map
    working on multiple sliders

--- js/template.js
+++ js/template.js
@@ -1,104 +1,101 @@
 var lastScrollTop = 0;
-var mapDescContainer = $("#kenya-maps-description");
+var mapDescContainer = $(".map-description");
+//nav bar height
+var navHeight = $("#mainNav").outerHeight();
+//window height
+var windowHeight = $(window).height();
+var mapContinerHeight = 100 - ((navHeight / windowHeight) * 100);
+//map container class height
+var mapContiner = $(".maps-container");
+mapContiner.height(windowHeight - navHeight);
 
-$(window).scroll(function (e) {
-    var mapContiner = $("#kenya");
-    var mapDescContainer = $("#kenya-maps-description");
-    var descText = $(".description-text");
-    var mapDiv = $("#kenya-maps");
-    var mapDivHeight = mapDiv.outerHeight();
-    var activeMap = 0;
-    var mapChilds = mapDiv.children('div').length;
-    var divTop = mapDiv.offset().top - 200;
-    var divBottom = divTop + mapDiv.outerHeight();
-    var height = $(window).scrollTop();
-    if (divTop < height && divBottom > height) {
-        var navHeight = $("#mainNav").outerHeight();
-        var windowHeight = $(window).height();
-        var kenyaMapsHeight = 100 - ((navHeight / windowHeight) * 100);
-        mapContiner.height(kenyaMapsHeight + "% !important");
-        mapContiner.css({
-            "height": kenyaMapsHeight + "% !important",
-        });
-        mapDescContainer.removeClass('d-none');
-        mapDescContainer.css({
-            "margin-top": "-" + 70 + "%",
-            'overflow': 'scroll',
-            "height": mapDivHeight + "px",
-            "background-color": "#6a6a6a80",
-            "opacity": 0.9,
-        });
-        descText.css({
-            "height": mapDivHeight + "px",
-        });
-    }
-    else if (divTop > height) {
-        mapDescContainer.addClass('d-none');
-    }
-    else if (divBottom < height) {
-        mapDescContainer.addClass('d-none');
-    }
-    else {
-        mapContiner.height("auto");
-    }
-
-});
 
 
 var position = mapDescContainer.scrollTop();
 var desc_id = 1;
 mapDescContainer.scroll(function (e) {
+    var target = e.target.closest;
+    //console.log(target);
     var height = $(this).scrollTop();
-    var mapChilds = mapDescContainer.children('div').length;
-    //console.log(mapChilds);
+    var mapChilds = $(this).children('div').length;
+    var courosel = $(this).parent().children('div')[0];//.children().children();
+    courosel = $("#" + courosel.getAttribute("id"));
+    //console.log(courosel);
 
     var direction = "scrollDown";
     if (height > position) {
-        //console.log('scrollDown');
         direction = "scrollDown";
     }
     else {
-        //console.log('scrollUp');
         direction = "scrollUp";
     }
     position = height;
 
-    /*mapChilds.each(function () {
-        var mapId = ($(this).data("map"));
-        if (isOnScreen($(this))) {
-            //console.log(mapId);
-            //$("#" + mapId).removeClass("d-none");
-        }
-        else {
-            //$("#" + mapId).addClass("d-none");
-        }
-    });*/
+    //id 1st element is past screen show second to nth use active class to keep track of scroll
 
-    //id 1st element is past screen show second to nth
-    var desc_item = $("#desc-" + desc_id);
+    var activeItem = $(this).children('div .active');
+    var itemsArray = $(this).children('div');
+    //console.log(Array.from(activeItem));
+    var couroselPosition = Array.from(itemsArray).indexOf(Array.from(activeItem)[0]);
 
-    if (isOnScreen(desc_item) == true) {
-        if (direction === "scrollUp") {
-            if (desc_id > 1) {
-                desc_id = desc_id - 1;
-                $('.carousel').carousel("prev");
+
+    //var desc_item = $("#desc-" + desc_id);
+    console.log(couroselPosition + " == " + mapChilds);
+    if (couroselPosition != -1 && couroselPosition < mapChilds) {
+        var desc_item = itemsArray[couroselPosition];
+        if (isOnScreen(desc_item, direction) == "next") {
+            if (couroselPosition < mapChilds) {
+                var newActive = itemsArray[couroselPosition + 1];
+                var oldActive = itemsArray[couroselPosition];
+                moveActiveDesc(newActive, oldActive);
+                console.log(courosel);
+                //desc_id = desc_id + 1;
+                courosel.carousel("next");
+                //$('.rai-components').carousel("next");
             }
+            //console.log(desc_id);
+            courosel.carousel("pause");
         }
-        else {
-            if (desc_id < mapChilds) {
-                desc_id = desc_id + 1;
-                $('.carousel').carousel("next");
+        if (isOnScreen(desc_item, direction) == "prev") {
+            if (couroselPosition > 0) {
+                var newActive = itemsArray[couroselPosition - 1];
+                var oldActive = itemsArray[couroselPosition];
+                moveActiveDesc(newActive, oldActive);
+                console.log(courosel);
+                //desc_id = desc_id - 1;
+                courosel.carousel("prev");
             }
-
+            //console.log(desc_id);
+            courosel.carousel("pause");
         }
-        console.log(desc_id);
-        $('.carousel').carousel("pause");
     }
 
 });
 
-function isOnScreen(element) {
-    console.log($("#" + element.attr("id") + ':visible').visible(false, true) + " id >" + element.attr("id"));
-    return $("#" + element.attr("id") + ':visible').visible(false, true);
-    //return elementBottom > viewportTop;//&& elementTop < viewportBottom;
+function isOnScreen(element, direction) {
+    //console.log($("#" + element.attr("id") + ':visible').visible(false, true) + " id >" + element.attr("id"));
+    //return $("#" + element.attr("id") + ':visible').visible(false, true);
+    //console.log(element);
+    var viewportTop = mapDescContainer.offset().top - 0;
+    var viewportBottom = viewportTop + mapDescContainer.outerHeight();
+    var elementTop = $(element).offset().top - 10;
+    var elementBottom = elementTop + $(element).outerHeight();
+    //console.log(viewportTop + "=> VT, " + viewportBottom + "=> VB, " + elementTop + "=>ElT, " + elementBottom + "=>ELB");
+
+    if (direction === "scrollUp") {
+        if (elementTop >= viewportTop) {
+            return "prev";
+        }
+    }
+    else {
+        if (elementBottom <= viewportTop) {
+            return "next";
+        };
+    }
+}
+
+function moveActiveDesc(newActive, oldActive) {
+    console.log(newActive);
+    oldActive.classList.remove('active');
+    newActive.classList.add('active');
 }
